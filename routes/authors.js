b4f62aa4b76f7edf4df9394c3e47eef49037f435
kissz89 <kissz89@gmail.com>
2021-12-19 20:13:56 +0100

    Added search options

--- routes/authors.js
+++ routes/authors.js
@@ -3,8 +3,19 @@ const router = express.Router()
 const Author = require('../models/authors')
  
 //All authors route
-router.get('/', (req,res) => {
-    res.render('authors/index')
+router.get('/', async (req, res) => {
+    let searchOptions = {}
+    if(req.query.name != null && req.query.name !== ''){
+        searchOptions.name = new RegExp(req.query.name, 'i')
+    }
+    try{
+        const authors = await Author.find(searchOptions)
+        res.render('authors/index', {
+            authors: authors, 
+            searchOptions: req.query})
+    }catch{
+        res.redirect('/')
+    }
 })
 
 //new authors
@@ -12,6 +23,7 @@ router.get('/new', (req,res) =>{
     res.render('authors/new', {author: new Author()})
 })
 
+//Create author route
 router.post('/', async (req,res) => {
     const author = new Author({
         name: req.body.name
