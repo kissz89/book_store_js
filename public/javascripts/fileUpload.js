9418c9df18e53deed91dcb7e42ad1458b0cadb41
kissz89 <kissz89@gmail.com>
2022-01-06 00:34:55 +0100

    File Upload Database

---
+++ public/javascripts/fileUpload.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function(){
+    FilePond.registerPlugin(
+    FilePondPluginImagePreview,
+    FilePondPluginImageResize,
+    FilePondPluginFileEncode,
+)
+
+FilePond.setOptions({
+    stylePanelAspectRatio: 150 / 100,
+    imageResizeTargetWidth: 100,
+    imageResizeTargetHeight: 150
+})
+FilePond.parse(document.body)
+})
